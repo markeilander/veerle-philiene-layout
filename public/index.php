9609537d5b09fb127f1a08f74759720ea729c454
Mark Eilander <mark.eilander@outlook.com>
2014-05-09 17:50:05 +0200

    correct config

--- public/index.php
+++ public/index.php
@@ -1,4 +1,17 @@
 <?php
 
-  echo '<h1>Veerle-philiene.nl</h1>';
-  echo 'test';
+  $app = new \Phalcon\Mvc\Micro();
+
+  //define the routes here
+//This is the start route
+$app->get('/', function () {
+    echo "<h1>Welcome!</h1>";
+});
+$app->get('/test', function () {
+    echo "<h1>Test url's!</h1>";
+});
+  $app->get('/say/hello/{name}', function ($name) {
+    echo "<h1>Hello! $name</h1>";
+ });
+
+  $app->handle();
